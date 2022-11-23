526f4376c30a4461a2a424675528e3759a4926ca
1024xx4 <teraleaf1024.4@gmail.com>
2022-11-23 19:26:51 +0900

    Test: 練習問題(5.7)#6

---
+++ chapter05/test_start_gen.py
@@ -0,0 +1,14 @@
+from cards import Card
+
+
+def pytest_generate_tests(metafunc):
+    if "start_state" in metafunc.fixturenames:
+        metafunc.parametrize("start_state", ["done", "in prog", "todo"])
+
+
+def test_start(cards_db, start_state):
+    c = Card("second edition", state=start_state)
+    index = cards_db.add_card(c)
+    cards_db.start(index)
+    card = cards_db.get_card(index)
+    assert card.state == "in prog"
