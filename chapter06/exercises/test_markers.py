9f316e26f6ca7e78288acefc346310c324cd2db8
1024xx4 <teraleaf1024.4@gmail.com>
2022-12-03 05:48:42 +0900

    Test: 練習問題をする(6.12)#8

---
+++ chapter06/exercises/test_markers.py
@@ -0,0 +1,32 @@
+import pytest
+
+pytestmark = pytest.mark.all
+
+
+@pytest.mark.odd
+def test_one():
+    pass
+
+
+def test_two():
+    pass
+
+
+@pytest.mark.odd
+def test_three():
+    pass
+
+
+@pytest.mark.testclass
+class TestClass:
+    def test_four(self):
+        pass
+
+    @pytest.mark.odd
+    def test_five(self):
+        pass
+
+
+@pytest.mark.parametrize("x", [6, 7])
+def test_param(x):
+    pass
